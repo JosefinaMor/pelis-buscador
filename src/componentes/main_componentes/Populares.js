5bfa4a49acf1fb18f43004911952a80b1ad9c8fa
JosefinaMor <josefina.morales.1765@gmail.com>
2022-04-08 09:17:27 -0300

    agrego mixins creados en mi trabajo para mi trabajo anterior para reutilizar botones de paginado

--- src/componentes/main_componentes/Populares.js
+++ src/componentes/main_componentes/Populares.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
+import { GrCaretNext, GrCaretPrevious } from 'react-icons/gr';
+
 
 const Populares = () => {
   const [arrayPelisPopulares, setArrayPelisPopulares] = useState([]);
+
   useEffect(() => {
     fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=4e1ae359e8d00fd3c5fa0742e3a2be5f&language=es-ES&page=1')
       .then(res => res.json())
       .then((cardData)=>{
-        console.log(cardData.results);
         setArrayPelisPopulares(cardData.results);
       })
   }, []);
@@ -20,10 +22,15 @@ const Populares = () => {
         <div className="card">
           <img src="http://placekitten.com/200/300" alt={pelicula.title} />
           <div>
-            <h1>{pelicula.title}</h1>
+            <h2>{pelicula.title}</h2>
           </div>
         </div>
         ))}
+        
+      </div>
+      <div className="prev-next-buttons">
+        <button id="prev-page-button" aria-label="previous page"><GrCaretPrevious /></button>
+        <button id="next-page-button" aria-label="next page"><GrCaretNext /></button>
       </div>
     </div>
   );
--- src/componentes/main_componentes/Ultimos_lanzamientos.js
+++ src/componentes/main_componentes/Ultimos_lanzamientos.js
@@ -4,23 +4,22 @@ const UltimosLanzamientos = () => {
 
     const [arrayUltimosLanzamientos, setArrayUltimosLanzamientos] = useState([]);
     useEffect(() => {
-      fetch('https://api.themoviedb.org/3/movie/latest?api_key=4e1ae359e8d00fd3c5fa0742e3a2be5f&language=es-ES&page=1')
+      fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=4e1ae359e8d00fd3c5fa0742e3a2be5f&language=es-ES&page=1')
         .then(res => res.json())
         .then((cardData)=>{
-          console.log(cardData.results);
           setArrayUltimosLanzamientos(cardData.results);
         })
     }, []);
   
     return (
       <div className="page">
-        <h1>Populares</h1>
+        <h1>Ultimos lanzamientos</h1>
         <div className="container">
           {arrayUltimosLanzamientos.map((pelicula) =>(
           <div className="card">
             <img src="http://placekitten.com/200/300" alt={pelicula.title} />
             <div>
-              <h1>{pelicula.title}</h1>
+              <h2>{pelicula.title}</h2>
             </div>
           </div>
           ))}
