1c65f46a269154e448b3c60880412bb2c8eafaff
JosefinaMor <josefina.morales.1765@gmail.com>
2022-04-08 08:53:07 -0300

    trabajando en renderizacion de paginado populares y ultimos lanzamientos

--- src/componentes/main_componentes/Populares.js
+++ src/componentes/main_componentes/Populares.js
@@ -1,8 +1,32 @@
+import { useState, useEffect } from "react";
+
 const Populares = () => {
-    
-    return (
-      <div>populares</div>
-    );
-  }
+  const [arrayPelisPopulares, setArrayPelisPopulares] = useState([]);
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=4e1ae359e8d00fd3c5fa0742e3a2be5f&language=es-ES&page=1')
+      .then(res => res.json())
+      .then((cardData)=>{
+        console.log(cardData.results);
+        setArrayPelisPopulares(cardData.results);
+      })
+  }, []);
+
+  
+  return (
+    <div className="page">
+      <h1>Populares</h1>
+      <div className="container">
+        {arrayPelisPopulares.map((pelicula) =>(
+        <div className="card">
+          <img src="http://placekitten.com/200/300" alt={pelicula.title} />
+          <div>
+            <h1>{pelicula.title}</h1>
+          </div>
+        </div>
+        ))}
+      </div>
+    </div>
+  );
+}
   
   export default Populares;
--- src/componentes/main_componentes/Ultimos_lanzamientos.js
+++ src/componentes/main_componentes/Ultimos_lanzamientos.js
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react";
+
 const UltimosLanzamientos = () => {
-    
+
+    const [arrayUltimosLanzamientos, setArrayUltimosLanzamientos] = useState([]);
+    useEffect(() => {
+      fetch('https://api.themoviedb.org/3/movie/latest?api_key=4e1ae359e8d00fd3c5fa0742e3a2be5f&language=es-ES&page=1')
+        .then(res => res.json())
+        .then((cardData)=>{
+          console.log(cardData.results);
+          setArrayUltimosLanzamientos(cardData.results);
+        })
+    }, []);
+  
     return (
-      <div>ultimos lanzamientos</div>
+      <div className="page">
+        <h1>Populares</h1>
+        <div className="container">
+          {arrayUltimosLanzamientos.map((pelicula) =>(
+          <div className="card">
+            <img src="http://placekitten.com/200/300" alt={pelicula.title} />
+            <div>
+              <h1>{pelicula.title}</h1>
+            </div>
+          </div>
+          ))}
+        </div>
+      </div>
     );
   }
   
