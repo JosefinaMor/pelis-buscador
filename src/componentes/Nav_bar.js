7c465d9d8064bc8a32650f1db7775e9f12ce1807
JosefinaMor <josefina.morales.1765@gmail.com>
2022-04-07 22:09:20 -0300

    trabajando en el Navbar

---
+++ src/componentes/Nav_bar.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import { GiFilmStrip } from 'react-icons/gi';
+
+const NavBar = () => {
+    
+    return (
+      <nav className="nav-bar">
+          <ul>
+              <li><GiFilmStrip /></li>
+              <li><Link to={'/'}>Home</Link></li>
+              <li><Link to={'/'}>Ultimos lanzamientos</Link></li>
+              <li><Link to={'/'}>Populares</Link></li>
+              <li><Link to={'/'}>Buscar</Link></li>
+              <li><Link to={'/'}>Buscar</Link></li>
+          </ul>
+      </nav>
+    );
+  }
+  
+  export default NavBar;
