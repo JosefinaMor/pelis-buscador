8a8129bfc8610a48d311ae615fa3d2e8d733dcdf
JosefinaMor <josefina.morales.1765@gmail.com>
2022-04-08 17:34:11 -0300

    trabajando en estilos de la landing page

--- src/componentes/main_componentes/Home.js
+++ src/componentes/main_componentes/Home.js
@@ -1,7 +1,53 @@
+import { useEffect, useState } from "react";
+import { BiShow } from "react-icons/bi";
+
 const Home = () => {
-    
+  const [arrayUltimosLanzamientos, setArrayUltimosLanzamientos] = useState([]);
+  const [pagina, setPagina] = useState(1);
+
+
+  const [arrayPelisPopulares, setArrayPelisPopulares] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=4e1ae359e8d00fd3c5fa0742e3a2be5f&language=es-ES&page=1')
+      .then(res => res.json())
+      .then((cardData)=>{
+        setArrayPelisPopulares(cardData.results);
+      })
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=4e1ae359e8d00fd3c5fa0742e3a2be5f&language=es-ES&page=${pagina}`)
+      .then(res => res.json())
+      .then((cardData)=>{
+        setArrayUltimosLanzamientos(cardData.results);
+        console.log(cardData.results)
+      })
+  }, [pagina]);
+
+
     return (
-      <div>home</div>
+      <div>
+        <div className="container-ult-lanzamiento">      
+          {arrayUltimosLanzamientos.map((pelicula) =>(
+          <div className="card">
+            <div>
+              <h2>{pelicula.title}</h2>
+              <BiShow />
+            </div>
+          </div>
+          ))}
+        </div>
+        <div className="container-popular">
+          {arrayPelisPopulares.map((pelicula, index) =>(
+          <div className="card">
+            <div>
+              <h2>{pelicula.title}</h2>
+            </div>
+          </div>
+          ))}
+        </div>
+      </div>
     );
   }
   
